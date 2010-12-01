903380287b6dc740b57ea0ec93fc75b74b4f179d
jbleuzen <zenzen77@gmail.com>
2010-12-01 20:38:36 +0100

    Updating README Changing version and adding url to update

--- index.php
+++ index.php
@@ -17,12 +17,13 @@ Plugin::setInfos(array(
     'id'          => 'wpdb_import',
     'title'       => __('WordPress DataBase Import'), 
     'description' => __('Import data from a WordPress DataBase into WolfCMS'), 
-    'version'     => '0.0.5', 
+    'version'     => '0.0.6', 
     'license'     => 'MIT',
     'author'      => 'Johan BLEUZEN',
     'require_wolf_version' => '0.7.0',
-//    'update_url'  => '',
-    'website'     => 'https://github.com/jbleuzen/Wolf_WPDB_Import'
+    'update_url'  => 'http://www.johanbleuzen.fr/wolfcms_update.xml',
+    'website'     => 'https://github.com/jbleuzen/Wolf_WPDB_Import',
+		'type'        => 'backend'
 ));
 
 /* Stuff for backend. */
