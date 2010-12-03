644914e2bd31577df04111ff9141b9e04b941ddc
jbleuzen <zenzen77@gmail.com>
2010-12-03 22:44:31 +0100

    Updating version and adding global variable

--- index.php
+++ index.php
@@ -13,20 +13,30 @@
  *
  */
 
+/**
+ * Root location where WordPress Import plugin lives.
+ */
+define('WPDB_ROOT', URI_PUBLIC.'wolf/plugins/wpdb_import');
+
+/**
+ * Version of the plugin
+ */
+define('WPDB_VERSION', '0.0.7');
+
 Plugin::setInfos(array(
     'id'          => 'wpdb_import',
     'title'       => __('WordPress DataBase Import'), 
-    'description' => __('Import data from a WordPress DataBase into WolfCMS'), 
-    'version'     => '0.0.6', 
+    'description' => __('Import data from a WordPress DataBase into WolfCMS.'), 
+    'version'     => '0.0.7', 
     'license'     => 'MIT',
-    'author'      => 'Johan BLEUZEN',
-    'require_wolf_version' => '0.7.0',
+    'author'      => 'Johan Bleuzen',
     'update_url'  => 'http://www.johanbleuzen.fr/wolfcms_update.xml',
     'website'     => 'https://github.com/jbleuzen/Wolf_WPDB_Import',
-		'type'        => 'backend'
+		'type'        => 'backend',
+    'require_wolf_version' => '0.7.0'
 ));
 
-/* Stuff for backend. */
+/* Adds the tab in admin */
 if (defined('CMS_BACKEND'))  {   
     Plugin::addController('wpdb_import', 'WP Import');
 }
