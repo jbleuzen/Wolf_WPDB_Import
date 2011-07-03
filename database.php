9af4f153b3ff70e2c6417e4356231e7ac1ee031b
Matthew Coleman <logical.zero@gmail.com>
2011-07-03 02:28:12 -0500

    Update to version 1.0.5. Bug fixes. Installation now works.

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php 
+
+/*
+ * Wordpress Database Import - Wordpress to WolfCMS importing plugin
+ *
+ * Copyright (c) 2010 Johan BLEUZEN  and  Matthew COLEMAN
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://github.com/spacez320/wpdb_import
+ */
+	
+?>
